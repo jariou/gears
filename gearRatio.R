4b4e976fcc8d044e35b689d22c1e5537dbdd20bc
Jacques Rioux <jacques.rioux@outlook.com>
2019-09-12 13:36:05 -0400

    Cleaned up the code. Added a print method. Added a stub for bestGearRatio

--- gearRatio.R
+++ gearRatio.R
@@ -1,39 +1,57 @@
-function (m, n=1, depth = 20) 
+bestGearRatio<-function (m, maxDepth, maxGear) 
+{
+    cat("\nNeed to implement to obtain the best ratio approximation with certain constraints\n")
+}
+
+gearRatio<-function (m, n = 1, depth = 20) 
 {
     if(m>n)
     {
-        top   = m
+        top    = m
         bottom = n
     }
     else
     {
-        top   = n
+        top    = n
         bottom = m
     }
-    goal       = top/bottom
-    factors    = double(depth+2)
-    remainders = double(depth+2)
-
-    for( i in 1:(depth+2))
+    
+    goal         = top / bottom
+    factorVec    = double(depth + 2)
+    remainderVec = double(depth + 2)
+    
+    for( i in 1:(depth + 2))
     {
-        factors[i]    = top%/%bottom
-        remainders[i] = top%%bottom
-        top    = bottom
-        bottom = remainders[i]
+        factorVec[i]    = top%/%bottom
+        remainderVec[i] = top%%bottom
+        top             = bottom
+        bottom          = remainderVec[i]
     }
     
-    gears    = c(0, 1, rev(factors[1:depth]))
-
-    for( i in 2+(1:depth))    
-    {
-        gears[i] = gears[i] * gears[i-1] + gears[i-2]
-    }
-    ratio = gears[depth+2]/gears[depth+1]
+    gearVec    = c(0, 1, rev(factorVec[1:depth]) )
     
-    list(
-         gears = c(gears[depth+2],gears[depth+1]), 
-         ratio = ratio, 
-         goal  = goal,
-         error = 100*(ratio - goal) / goal
-         )
+    for( i in 2 + (1:depth))    
+    {
+        gearVec[i] = gearVec[i] * gearVec[i - 1] + gearVec[i - 2]
+    }
+    
+    ratio = gearVec[depth + 2] / gearVec[depth + 1]
+    
+    tmp = list(
+                gears        = c(gearVec[depth + 2], gearVec[depth + 1]), 
+                ratio        = ratio, 
+                goal         = goal,
+                percentError = 100 * (ratio - goal) / goal
+                )
+    
+    class(tmp) <- "gearRatio"
+    tmp
+}
+
+print.gearRatio<-function (x, ...) 
+{
+    cat("\nGears  :  ", x$gears[1], ":", x$gears[2], "\n")
+    cat("Ratio  :  ", format(x$ratio, digits=4), "\n")
+    cat("Error  :  ",  format(x$goal, digits=4), " [", paste(format(x$percentError,digits=3), "%",sep=""),"]\n\n")
+    invisible(x)
 }
